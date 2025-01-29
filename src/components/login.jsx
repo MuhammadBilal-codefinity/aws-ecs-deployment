702615b128d39fbdf888f9e22a48016b98f388dc
Muhammad Bilal <muhammad.bilal@codefinitysol.com>
2025-01-29 21:12:06 +0500

    gui updated

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,50 +5,66 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [formMessage, setFormMessage] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const onSubmit = async(event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
     });
     const data = await response.json();
     if (data.success) {
-      navigate('/users')
+      navigate("/users");
       // Navigate to the users page
     } else {
-      if(!username){
+      if (!username) {
         setFormMessage("Username is required");
       }
-      if(!password){
+      if (!password) {
         setFormMessage("Password is required");
       }
-      if(username === 'wronguser' && password === 'wrongpass'){
+      if (username === "wronguser" && password === "wrongpass") {
         setFormMessage("Invalid credentials");
       }
     }
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={onSubmit}>
-        {formMessage && <p>{formMessage}</p>}
-        <input
-          type="text"
-          placeholder="Username"
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div
+      style={{
+        backgroundColor: "#f2f2f2",
+        padding: "20px",
+        minHeight: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <h1>Login Page</h1>
+
+        <form onSubmit={onSubmit} style={{ display: "flex", flexDirection: "column" }}>
+          {formMessage && <p>{formMessage}</p>}
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+            style={{ marginBottom: "10px" , height:"20px" , padding:"5px" }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            style={{ marginBottom: "10px"  , height:"20px" , padding:"5px"}}
+          />
+          <button type="submit"
+          style={{ backgroundColor: "#4CAF50", color: "white", padding: "10px 20px", border: "none", cursor: "pointer" }}
+          >Login</button>
+        </form>
+      </div>
     </div>
   );
 };
